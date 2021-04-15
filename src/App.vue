d04b54e18ccdd12dadfee1882ec0dae18da8bf13
Kyleah <krfrederic@gmail.com>
2021-04-15 09:13:43 -0500

    flexbox and table border added

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  margin-top: 60px;
+  /* text-align: center; */
+  /* margin-top: 60px; */
 }
 
 body {
--- src/components/Generate.vue
+++ src/components/Generate.vue
@@ -1,21 +1,31 @@
 <template>
-  <div id="generate">
-    <button id="button" @click="handler()">Generate</button>
-    <table v-show="isGenerated">
-      <tr>
-        <td>Genre:</td>
-        <td>{{ genres }}</td>
-      </tr>
-      <tr>
-        <td>Setting:</td>
-        <td>{{ settings }}</td>
-      </tr>
-            <tr>
-        <td>Main Character:</td>
-        <td>{{ age }} / {{ genders }} / {{ charTraits }}</td>
-      </tr>
-    </table>
-  </div>
+  <section id="generate">
+    <div>
+      <button id="button" @click="handler()">Generate</button>
+    </div>
+    <div id="table">
+      <table v-show="isGenerated">
+        <tr>
+          <td>Genre:</td>
+          <td>{{ genres }}</td>
+        </tr>
+        <tr>
+          <td>Starting Setting:</td>
+          <td>{{ settings }}</td>
+        </tr>
+        <tr>
+          <td>Main Character:</td>
+          <td>
+          <ul>
+            <li>Age: {{ age }}</li>
+            <li>Gender: {{ genders }}</li>
+            <li>Character Trait: {{ charTraits }}</li>
+          </ul>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -69,10 +79,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #generate {
-  text-align: center;
+  /* text-align: center; */
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  justify-content: space-between;
 }
 #button {
   font-size: 80px;
@@ -80,13 +95,15 @@ export default {
   background-color: #76cef2;
   border: 4px solid #58efec;
 }
-table {
+#table {
+  /* table-layout: fixed; */
+  /* width: 50%; */
   font-size: 40px;
-  margin-left: auto;
-  margin-right: auto;
+  /* margin-left: auto;
+  margin-right: auto; */
   border-collapse:collapse;
-  border:4px solid #b18dff;
-  margin-top: 60px;
+  border:4px solid #000000;
+  border-spacing: 0;
 }
 tr:nth-child(even) {
   background-color: #93aef9;
@@ -95,6 +112,8 @@ tr:nth-child(odd) {
   background-color: #85bef6;
 }
 table td {
+  /* border:4px solid #b18dff; */
   border: 4px solid #a29dfc;
+  padding: 8px;
 }
 </style>
