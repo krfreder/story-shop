b52cb51d839982f8364499b4e5d0c349408d925a
Kyleah <krfrederic@gmail.com>
2021-04-14 20:54:48 -0500

    basic styling added

--- src/components/Generate.vue
+++ src/components/Generate.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="generate">
     <button id="button" @click="handler()">Generate</button>
-    <h1 v-show="isGenerated">Genre: {{ genres }}</h1>
+    <table v-show="isGenerated">
+      <tr>
+        <td>Genre:</td>
+        <td>{{ genres }}</td>
+      </tr>
+      <tr>
+        <td>Setting:</td>
+        <td>{{ settings }}</td>
+      </tr>
+            <tr>
+        <td>Main Character:</td>
+        <td>{{ age }} / {{ genders }} / {{ charTraits }}</td>
+      </tr>
+    </table>
+    <!-- <h1 v-show="isGenerated">Genre: {{ genres }}</h1>
     <h1 v-show="isGenerated">Setting: {{ settings }}</h1>
-    <h1 v-show="isGenerated">Main Character: {{ age }} / {{ genders }} / {{ charTraits }}</h1>
+    <h1 v-show="isGenerated">Main Character: {{ age }} / {{ genders }} / {{ charTraits }}</h1> -->
   </div>
 </template>
 
@@ -56,11 +70,30 @@ export default {
 <style>
 #generate {
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 #button {
   font-size: 80px;
   border-radius: 10px;
+  background-color: #76cef2;
+  border: 4px solid #58efec;
+}
+table {
+  font-size: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  border-collapse:collapse;
+  border:4px solid #b18dff;
+  margin-top: 60px;
+}
+tr:nth-child(even) {
+  background-color: #93aef9;
+}
+tr:nth-child(odd) {
+  background-color: #85bef6;
+}
+table td {
+  border: 4px solid #a29dfc;
+  /* border:4px solid #b18dff; */
 }
 </style>
