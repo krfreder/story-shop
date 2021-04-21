ac003d3bc46bb83af61bfa9787e6a1a627ae4f3a
Kyleah <krfrederic@gmail.com>
2021-04-21 10:06:45 -0500

    added logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>One Stop Story Prompts</h1>
+    <img src="../assets/ossp_logo.png" alt="Text logo reading One Stop Story Prompts"/>
 </template>
+
+<style scoped>
+img {
+  width: auto;
+  height: 100%;
+  max-height: 100vh;
+}
+</style>
